e3af542c3589b093a4c53c23de894fc2bb9ab561
always-awake <leemirim9654@gmail.com>
2019-09-17 12:10:56 +0900

    [INIT] eslint 설정 1. airbnb-base를 기준으로 코드 스타일 체크하도록 설정 2. airbnb-base의 일부 규칙 변경(커스터마이징)

---
+++ web/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "browser": true,
+    "es6": true
+  },
+  "extends": "airbnb-base",
+  "rules": {
+    "indent": [
+      "error",
+        2
+      ],
+  "linebreak-style": [
+    "error",
+    "unix"
+  ],
+  "quotes": [
+    "error",
+    "single"
+  ],
+  "semi": [
+    "error",
+    "always"
+  ]},
+  "parserOptions": {
+    "sourceType": "module"
+  },
+};
