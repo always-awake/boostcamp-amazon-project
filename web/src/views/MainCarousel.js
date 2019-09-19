f8fd884e990b066f1096f1253d9d9b90331961bf
always-awake <leemirim9654@gmail.com>
2019-09-19 15:58:30 +0900

    [#9][ADD] 메인 캐로셀 카드 부분 이벤트 구현
    1. 카드를 클릭했을 때 해당 카드가 커지는 이벤트 구현
    2. 클릭된 카드가 크기가 커져도 글씨에는 변화가 없도록 구현
    3. 다른 카드를 클릭했을 때 이전에 클릭했던 카드는 다시 작아지도록 구현

--- web/src/views/MainCarousel.js
+++ web/src/views/MainCarousel.js
@@ -1,5 +1,6 @@
 import mainCarouselComponent from '../components/main-carousel/main-carousel';
 import setCarouselEvent from '../setEvents/carouselEvent';
+import setCardsEvent from '../setEvents/main-carousel/cardEvent';
 
 class MainCarousel {
   constructor(parentDivId) {
@@ -18,6 +19,7 @@ class MainCarousel {
   setEvent() {
     const mainCarousel = this.mainCarouselArticle.querySelector('.main__carousel');
     setCarouselEvent.setCarouselEvent(mainCarousel);
+    setCardsEvent.setCardsEvent();
   }
 }
 
