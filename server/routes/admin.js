a104f76f2c526dca841d170acaa64fc55e24d00f
always-awake <leemirim9654@gmail.com>
2019-09-24 12:57:43 +0900

    [#21] db에서 관리자 페이지의 유저 목록 데이터 가져오기
    amazon 데이터베이스의 users 테이블에 있는 모든 유저 목록 조회

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,10 +5,25 @@ const mysql = require('mysql2');
 
 const connection = mysql.createPool(dbConfig);
 
+router.get('/', function(req, res) {
+  res.send('어드민 페이지');
+});
 
 // 메인화면
 router.get('/main', function(req, res) {
   connection.query('SHOW TABLES;', function(err, rows, fields) {
+    if (!err) {
+      console.log('The solution is: ', rows);
+      res.send(rows);
+    } else {
+      console.log('Error while performing Query.', err);
+    }
+  });
+});
+
+// 유저 목록 조회
+router.get('/users', function(req, res) {
+  connection.query('SELECT * FROM users;', function(err, rows) {
     if (!err) {
       console.log('The solution is: ', rows);
       res.send(rows);
