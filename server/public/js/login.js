1dd4fb540e86215c8749569b9d7b8ceaec73aff8
always-awake <leemirim9654@gmail.com>
2019-09-25 21:11:34 +0900

    [#29][ADD] 관리자 페이지 로그인 UI 구현
    1. 다른 페이지와 중복되는 css 코드는 common.css를 import 하여 적용
    2. 로그인 버튼을 클릭했을 때, 로그인 로직이 실행되도록 로그인 버튼에 이벤트 설정
    3. 헤더 부분의 로그아웃 버튼을 클릭했을 때, 로그아웃 로직이 실행되도록 로그아웃 버튼에 이벤트 설정

---
+++ server/public/js/login.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+  setLoginEvent();
+};
+
+const setLoginEvent = () => {
+  const loginSection = document.querySelector('.login__section');
+  const loginButton = loginSection.querySelector('.login__button');
+  loginButton.addEventListener('click', () => {
+    const loginForm = loginSection.querySelector('.login__form');
+    loginForm.submit();
+  });
+};
+
